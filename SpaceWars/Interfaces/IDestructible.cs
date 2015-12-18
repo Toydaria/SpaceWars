21bfe6fb118fe8b6570582457f3d7889f200bd3e
stefkay <skaykieva@gmail.com>
2015-12-18 14:50:08 +0200

    deleted MaxHealth

--- SpaceWars/Interfaces/IDestructible.cs
+++ SpaceWars/Interfaces/IDestructible.cs
@@ -4,8 +4,7 @@ namespace SpaceWars.Interfaces
     public interface IDestructibleObject
     {
         int Health { get; set; }
-        int MaxHealth { get; set; }
-
+        
         void TakeDMG(int dmg);
         void Destroy();
     }
