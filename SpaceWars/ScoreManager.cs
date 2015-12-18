cb87fe9324ab126a65e6e7a637b2c6fd78b47a75
stefkay <skaykieva@gmail.com>
2015-12-18 14:06:56 +0200

    added ScoreManager class

---
+++ SpaceWars/ScoreManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SpaceWars.Interfaces;
+
+namespace SpaceWars
+{
+    class ScoreManager
+    {
+        private int totalScore;
+
+        public ScoreManager(int totalScore)
+        {
+            this.TotalScore = totalScore;
+        }
+        
+        public int TotalScore
+        {
+            get { return this.totalScore; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Score cannot be negative");
+                }
+            }
+        }
+
+        public void AddPoints(IGiveScore enemyTarget)
+        {
+            this.TotalScore += enemyTarget.Points;
+        }
+    }
+}
+
