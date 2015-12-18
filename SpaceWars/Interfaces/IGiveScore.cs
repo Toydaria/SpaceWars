a72a26e078fb54a71d7029f2a2b5ecf0e3443ad1
stefkay <skaykieva@gmail.com>
2015-12-18 14:50:55 +0200

    new interface related to score

---
+++ SpaceWars/Interfaces/IGiveScore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceWars.Interfaces
+{
+    public interface IGiveScore
+    {
+        int ScoringPoints { get; }
+    }
+}
